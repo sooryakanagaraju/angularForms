062a058b830f1eddca243533545aaf76c1a29d0e
soorya <sooryakanagaraj11@gmail.com>
2025-01-01 11:16:18 +0530

    WIP

--- learnBasic/src/app/app.component.ts
+++ learnBasic/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {COURSES, titles} from './db-data';
+import {Course} from './models/course';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CourseCardComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learnBasic';
+  courses = COURSES;
+  angularCourse:Course = COURSES[0];
+  rxjsCourse:Course = COURSES[1];
+  ngrxCourse:Course = COURSES[2];
+
+  onCourseSelected(course:Course) {
+    console.log('this is the card selected in the app component', course);
+  }
 }
